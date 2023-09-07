ee6055d8040c60ad85fd74dd27bc5193219d3c17
Levi John Favour <engrlevijohn@gmail.com>
2023-09-08 00:37:36 +0100

    second commit

--- src/components/service/Services.jsx
+++ src/components/service/Services.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import './services.css';
+import { BiCheck } from 'react-icons/bi';
 
 const Services = () => {
-  return <section id='services'>services</section>;
+  return (
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+
+            <ul>
+              <li>
+                <BiCheck className='service__list-icon' />
+                <p>Ux writing</p>
+              </li>
+              <li>
+                <BiCheck className='service__list-icon' />
+                <p>Ux writing</p>
+              </li>
+              <li>
+                <BiCheck className='service__list-icon' />
+                <p>Ux writing</p>
+              </li>
+              <li>
+                <BiCheck className='service__list-icon' />
+                <p>Ux writing</p>
+              </li>
+              <li>
+                <BiCheck className='service__list-icon' />
+                <p>Ux writing</p>
+              </li>
+            </ul>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
